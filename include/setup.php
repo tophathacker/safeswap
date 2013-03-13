91e776a27c433e11cfb54eb02cfc8cc5c261e143
Ryan Hatfield <tophathacker@gmail.com>
2013-03-13 14:31:36 -0500

    fixed some redundant code

--- include/setup.php
+++ include/setup.php
@@ -2,13 +2,11 @@
 /* here in the setup file, check for config and if it's there run with it. Otherwise, create a new config based on default-config.php */
 
 require(ROOTPATH . 'include/dbutil.php');
-if(file_exists(ROOTPATH . 'config/config.php')){
-  require(ROOTPATH . 'config/config.php');
-}
-else{
+require(ROOTPATH . 'include/utils.php');
+
+if(!file_exists(ROOTPATH . 'config/config.php'))
   copy(ROOTPATH . 'config/default-config.php', ROOTPATH . 'config/config.php');
-  require(ROOTPATH . 'config/default-config.php');
-}
+require(ROOTPATH . 'config/default-config.php');
 
 //i'll eventually get this from config.php file
 $database = new db_object('localhost','safeswap','safeswap','ss_','safeswap');
